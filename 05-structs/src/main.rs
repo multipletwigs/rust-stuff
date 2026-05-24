b8c80c9244cd27f71631f78f9dc041119d9eeb4e
bashtwigs <zachkhong.workplace@gmail.com>
2026-05-24 14:48:48 +0800

    can_hold

--- 05-structs/src/main.rs
+++ 05-structs/src/main.rs
@@ -8,6 +8,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+
+    fn square(&self) -> Rectangle {
+        return Rectangle {
+            width: self.height,
+            height: self.height,
+        };
+    }
 }
 
 fn main() {
